2c3b394ecb3de045c1832387fed0c537e4ae097c
Vitto2623 <vtr.1@icloud.com>
2022-05-19 18:43:20 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
     Route::get('/', 'HomeController@index' )->name('home');
